1f361805d1a2a2c360d3ba35883a7fe81dcfa7db
Rian1010 <rtegally1098@gmail.com>
2020-06-17 23:54:53 +0200

    updated shipping bag template

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')), # URLs for login, logout, password-resets, etc.
     path('', include('home.urls')),
     path('products/', include('products.urls')),
+    path('bag/', include('bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
