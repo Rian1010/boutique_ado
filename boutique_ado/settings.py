d930fe2993164a3871e0ce6932797c044a08cb4f
Rian1010 <rtegally1098@gmail.com>
2020-06-29 18:48:17 +0200

    Trying to solve a bug

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -193,7 +193,10 @@ if 'USE_AWS' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_DEFAULT_ACL = None
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000'
+    }
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
