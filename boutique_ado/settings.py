de1368c2726308b531a2085f20f85508e4422dba
Rian1010 <rtegally1098@gmail.com>
2020-06-30 18:52:58 +0200

    Trying to solve a bug

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -220,10 +220,11 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
+# DEFAULT_FROM_EMAIL = 'rtegally1098@gmail.com' 
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'rtegally1098@gmail.com'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
